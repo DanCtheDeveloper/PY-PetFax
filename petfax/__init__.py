7a1c61a6f8e5e4b9b6143b4f92e8f6742d7a5fde
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-09-25 20:19:26 -0700

    Completed Integration with Flask and SQL

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,9 +1,18 @@
 # config
 from flask import Flask
+from flask_migrate import Migrate
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1Mississippi*@localhost:5432/PetFax'
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     # index route
     @app.route('/')
--- petfax/facts.py
+++ petfax/facts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import (Blueprint, render_template, request, redirect)
 
 # Define a Blueprint for the facts routes
 bp = Blueprint('fact', __name__, url_prefix="/facts")
@@ -7,3 +7,13 @@ bp = Blueprint('fact', __name__, url_prefix="/facts")
 @bp.route('/new')
 def new():
     return render_template('facts/new.html')
+
+# Define the route for listing facts
+
+@bp.route('/', methods=['GET', 'POST']) 
+def index(): 
+    if request.method == 'POST':
+       print(request.form)
+       return redirect('/facts')
+    
+    return render_template('facts/index.html')
